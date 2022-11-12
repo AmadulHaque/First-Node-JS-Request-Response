e44cc7251a53cdb8800e455ed87b8eb11aa0f571
AmadulHaque <mdamadulhaque8@gmail.com>
2022-11-12 20:36:50 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let http=require('http');
+let fs=require('fs');
+var server=http.createServer(function (req,res) {
+    if(req.url=="/"){
+        let data= fs.readFileSync('home.html','utf8');
+        res.end(data);
+    }else if(req.url=="/about"){
+        let data= fs.readFileSync('about.html','utf8');
+        res.end(data);
+    }else if(req.url=="/contact"){
+        let data= fs.readFileSync('contact.html','utf8');
+        res.end(data);
+    }
+});
+server.listen(8080);
+console.log("Server Run Success");
